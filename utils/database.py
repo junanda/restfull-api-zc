524d7e4aa34855d1aeeaeb0b13eedd8231ca6c57
anzoid <jpatihullah@gmail.com>
2021-08-15 00:52:08 +0700

    update connection

---
+++ utils/database.py
@@ -0,0 +1,17 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABSE_URL = "mariadb+pymysql://{}:{}@{}/{}?charset=utf8mb4".format(os.getenv('MARIADB_USERNAME'),
+                                                                                os.getenv('MARIADB_PASSWORD'),
+                                                                                os.getenv('MARIADB_HOST'),
+                                                                                os.getenv('DB_NAME'))
+
+engine = create_engine(
+    SQLALCHEMY_DATABSE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
